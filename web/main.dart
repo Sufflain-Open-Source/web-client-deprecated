220972472828f92f8629058a42f92385f7701f18
Timofey Chuchkanov <crt0r.9@yahoo.com>
2021-10-06 22:05:30 +0300

    feat(ui): sign in error ui message

--- web/main.dart
+++ web/main.dart
@@ -22,34 +22,24 @@ import 'package:web_client/app/auth.dart';
 import 'package:web_client/remote_database_config.dart' as config;
 import 'package:web_client/app/app.dart';
 import 'package:web_client/core/use_cases/navigate_pages/navigate_pages_bloc.dart';
-import 'package:web_client/ui/components/error_message.dart';
 
 void main() {
-  final appInitErrorMessage = 'Произошла ошибка при инициализации приложения';
   late final firebaseApp;
   late final authenticator;
 
   var errorMessageElement = '';
 
-  try {
-    firebaseApp = initializeApp(
-        apiKey: '',
-        authDomain: config.authDomain,
-        databaseURL: config.databaseURL,
-        projectId: config.projectId,
-        storageBucket: config.storageBucket);
+  firebaseApp = initializeApp(
+      apiKey: config.apiKey,
+      authDomain: config.authDomain,
+      databaseURL: config.databaseURL,
+      projectId: config.projectId,
+      storageBucket: config.storageBucket);
 
-    authenticator = Authenticator(firebaseApp);
-    errorMessageElement = authenticator.anonymousSignIn();
-
-  } catch (e) {
-    print(appInitErrorMessage);
-    errorMessageElement =
-        makeMessage('error-message', 'app-init-error', appInitErrorMessage);
-  }
+  authenticator = Authenticator(firebaseApp);
+  errorMessageElement = authenticator.anonymousSignIn();
 
   if (errorMessageElement == '') {
-    
     final navigatePagesBloc = NavigatePagesBloc();
     final initApp = InitializeApp(navigatePagesBloc);
 
